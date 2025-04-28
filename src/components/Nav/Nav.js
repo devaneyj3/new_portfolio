87b2f387d8242a1d7d6d8dada6f01aefee9dfedc
devaneyj3 <jordandevaney28@gmail.com>
2025-04-28 13:12:26 -0400

    make navbar reponsive

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,56 +1,56 @@
-import React from "react";
+"use client"; // if you're using Next.js App Router
+
+import React, { useState } from "react";
 import styles from "./Nav.module.scss";
 import Link from "next/link";
-import RadiantButton from "../RadiantButton/RadiantButton";
 
 export default function Nav() {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggleMenu = () => {
+		setIsOpen(!isOpen);
+	};
+
 	return (
 		<nav className={styles.nav}>
-			<ul>
-				<li className={styles.current}>
-					<Link
-						className={styles.smoothscroll}
-						href="#home"
-						aria-label="Go to Home section">
-						Home
-					</Link>
-				</li>
-				<li>
-					<Link
-						className={styles.smoothscroll}
-						href="#about"
-						aria-label="Go to About section">
-						About
-					</Link>
-				</li>
-				<li>
-					<Link
-						className={styles.smoothscroll}
-						href="#services"
-						aria-label="Go to Services section">
-						Services
-					</Link>
-				</li>
-				<li>
-					<Link
-						className={styles.smoothscroll}
-						href="#resume"
-						aria-label="Go to Resume section">
-						Resume
-					</Link>
-				</li>
-				<li>
-					<Link
-						className={styles.smoothscroll}
-						href="#portfolio"
-						aria-label="Go to Works section">
-						Works
-					</Link>
-				</li>
-				<li>
-					<RadiantButton text="Contact" className={styles.nav_btn} />
-				</li>
-			</ul>
+			<div className={styles.nav_container}>
+				<div className={styles.hamburger} onClick={toggleMenu}>
+					{isOpen ? "✖" : "☰"}
+				</div>
+
+				<ul className={`${styles.nav_links} ${isOpen ? styles.active : ""}`}>
+					<li className={styles.current}>
+						<Link href="#home" aria-label="Go to Home section">
+							Home
+						</Link>
+					</li>
+					<li>
+						<Link href="#about" aria-label="Go to About section">
+							About
+						</Link>
+					</li>
+					<li>
+						<Link href="#services" aria-label="Go to Services section">
+							Services
+						</Link>
+					</li>
+					<li>
+						<Link href="#resume" aria-label="Go to Resume section">
+							Resume
+						</Link>
+					</li>
+					<li>
+						<Link href="#portfolio" aria-label="Go to Works section">
+							Works
+						</Link>
+					</li>
+					<li>
+						<Link href="#contact" aria-label="Go to Contact section">
+							Contact
+						</Link>
+					</li>
+				</ul>
+			</div>
 		</nav>
 	);
 }
