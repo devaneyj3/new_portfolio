38fa4702cfd085d23af1c97d388c5b3655979cd9
devaneyj3 <jordandevaney28@gmail.com>
2025-05-12 21:04:49 -0400

    import colors scss file and make articleLayout

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -2,22 +2,22 @@
 import React, { useEffect, useState } from "react";
 
 import classes from "./Blog.module.scss";
-import * as contentful from "contentful";
+// import * as contentful from "contentful";
 
-const client = contentful.createClient({
-	space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE, // Replace with your space ID
-	accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_CONTENT_DELIVERY_TOKEN, // Replace with your access token
-});
+// const client = contentful.createClient({
+// 	space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE, // Replace with your space ID
+// 	accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_CONTENT_DELIVERY_TOKEN, // Replace with your access token
+// });
 
 export default function Blog() {
 	const [posts, setPosts] = useState([]);
-	useEffect(() => {
-		const getData = async () => {
-			const data = await client.getEntries();
-			setPosts(data.items);
-		};
-		getData();
-	}, []);
+	// useEffect(() => {
+	// 	const getData = async () => {
+	// 		const data = await client.getEntries();
+	// 		setPosts(data.items);
+	// 	};
+	// 	getData();
+	// }, []);
 
 	return (
 		<div className={classes.container}>
